46d0b2869d0309f379a763ab6c1cd8c2c3b58fb1
bert <btemme@hotmail.com>
2017-10-03 16:38:16 +0200

    Step 1: Get locations from SensorThings server to draw on map

---
+++ ArcSensor/Button1.cs
@@ -0,0 +1,25 @@
+﻿using ArcGIS.Desktop.Framework.Contracts;
+using Newtonsoft.Json;
+using SensorThings.Client;
+using System.Windows;
+
+namespace ArcSensor
+{
+    internal class Button1 : Button
+    {
+        protected override void OnClick()
+        {
+            var server = "http://black-pearl:8080/v1.0";
+            var client = new SensorThingsClient(server);
+            var locations = client.GetLocationCollection().Items;
+
+            var firstfeature = locations[0].Feature;
+            var p = JsonConvert.DeserializeObject<GeoJSON.Net.Geometry.Point>(firstfeature.ToString());
+
+            MessageBox.Show($"location: {p.Coordinates.Longitude} , {p.Coordinates.Latitude}");
+
+            // todo: draw locations in map
+
+        }
+    }
+}
